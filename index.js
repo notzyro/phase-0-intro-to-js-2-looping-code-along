0f12596af6d2b07ec2760061dd57d2999c14f713
Edward Angelo Salas <zyro670@gmail.com>
2022-03-26 00:14:31 -0400

    commit tree a15e409c85d3389ea4473c8f9e6be92c7ccad0be

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards() {
+    var msg = [
+        "Thank you, Guadalupe, for the wonderful surprise gift!",
+        "Thank you, Ollie, for the wonderful surprise gift!",
+        "Thank you, Aki, for the wonderful surprise gift!",
+    ];
+    return msg;
+}
+
+let count = 0;
+function countDown() {
+    for (let countDown = 0; countDown < 11; countDown++) {
+        console.log(countDown);
+      }
+}
